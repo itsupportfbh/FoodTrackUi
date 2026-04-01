1ce5f88dcc4215c9bdc347d3b179fd77849fb89d
User <vjvimala123@gmail.com>
2026-04-01 10:58:01 +0530

    ccUI

--- src/app/main/catering-routing.module.ts
+++ src/app/main/catering-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CompanyMasterComponent } from './company-master/company-master.component';
 import { MealPlanComponent } from './meal-plan/meal-plan.component';
 import { DailyOrderComponent } from './daily-order/daily-order.component';
-import { ScannerComponent } from './scanner/scanner.component';
+
 import { ReportsComponent } from './reports/reports.component';
 import { BillingComponent } from './billing/billing.component';
 
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'company-master', component: CompanyMasterComponent },
   { path: 'meal-plan', component: MealPlanComponent },
   { path: 'daily-order', component: DailyOrderComponent },
-  { path: 'scanner', component: ScannerComponent },
+ 
   { path: 'reports', component: ReportsComponent },
   { path: 'billing', component: BillingComponent },
   { path: '', pathMatch: 'full', redirectTo: 'company-master' }
