d586cae302723f968e5692ca3b03c65702493017
AnusuyaMuthu <anu014634@gmail.com>
2026-04-23 22:11:41 -0700

    Changes

--- src/app/main/request-override/request-override.service.ts
+++ src/app/main/request-override/request-override.service.ts
@@ -1,85 +1,38 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
 
-export interface RequestOverrideHeader {
-  requestHeaderId: number;
-  requestNo: string;
-  companyId: number;
-  fromDate: string;
-  toDate: string;
-  overrideDate: string;
-  notes: string;
-}
-
-export interface RequestOverrideLine {
-  requestOverrideDetailId: number;
-  requestDetailId: number;
-  sessionId: number;
-  cuisineId: number;
-  locationId: number;
-  baseQty: number;
-  overrideQty: number;
-  isCancelled: boolean;
-}
-
-export interface RequestOverrideScreen {
-  header: RequestOverrideHeader;
-  lines: RequestOverrideLine[];
-}
-
-export interface SaveRequestOverride {
-  requestHeaderId: number;
-  fromDate: string;
-  toDate:string;
-  notes: string;
-  createdBy: number;
-  lines: SaveRequestOverrideLine[];
-}
-
-export interface SaveRequestOverrideLine {
-  requestDetailId: number;
-  sessionId: number;
-  cuisineId: number;
-  locationId: number;
-  baseQty: number;
-  overrideQty: number;
-  isCancelled: boolean;
-}
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class RequestOverrideService {
-  private baseUrl = `${environment.apiUrl}/RequestOverride`;
+  private apiUrl = `${environment.apiUrl}/RequestOverride`;
 
   constructor(private http: HttpClient) {}
 
   getScreen(requestHeaderId: number, fromDate: string, toDate: string): Observable<any> {
     const params = new HttpParams()
-      .set('requestHeaderId', requestHeaderId.toString())
+      .set('requestHeaderId', requestHeaderId)
       .set('fromDate', fromDate)
       .set('toDate', toDate);
 
-    return this.http.get<any>(`${this.baseUrl}/screen`, { params });
+    return this.http.get<any>(`${this.apiUrl}/GetScreenData`, { params });
   }
 
-  save(payload: SaveRequestOverride): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/save`, payload);
+  save(payload: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/Save`, payload);
   }
-  //  getOverrideList(requestHeaderId: number): Observable<any> {
-  //   const params = new HttpParams()
-  //     .set('requestHeaderId', requestHeaderId.toString());
 
-  //   return this.http.get<any>(`${this.baseUrl}/list`, { params });
-  // }
-
-  deleteOverride(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.baseUrl}/${id}`);
+  getOverrideList(companyId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/GetOverrideList?companyId=${companyId}`);
   }
-  getOverrideList(companyId: number) {
-  return this.http.get<any>(`${this.baseUrl}/list?companyId=${companyId}`);
-}
 
-getOverrideLines(requestOverrideId: number) {
-  return this.http.get<any>(`${this.baseUrl}/lines/${requestOverrideId}`);
-}
+  getOverrideLines(requestOverrideId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/GetOverrideLines?requestOverrideId=${requestOverrideId}`);
+  }
+
+  delete(id: number, updatedBy: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/Delete/${id}/${updatedBy}`);
+  }
 }
