36b384daaf4416751d496e3e715ee42d81bfea02
venumuthusamy <venumuthusamy@gmail.com>
2026-04-24 12:19:27 +0530

    order changes

--- src/app/main/request/request-service.ts
+++ src/app/main/request/request-service.ts
@@ -54,4 +54,9 @@ getLatestSiteSetting(): Observable<any> {
 getDefaultPlanRates(): Observable<any> {
   return this.http.get<any>(`${environment.apiUrl}/CuisinePrice/GetDefaultPlanRates`);
 }
+getPlanUserCounts(companyId: number): Observable<any> {
+  return this.http.get<any>(
+    `${this.baseUrl}/GetPlanUserCounts?companyId=${companyId}`
+  );
+}
 }
